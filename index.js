99c152512651b57f9c86669faaa08e95c48ec4d8
guilherme munis <gmmunis@hotmail.com>
2020-09-04 18:40:22 -0300

    aplicando os cálculos e as condições das notas dos alunos

--- index.js
+++ index.js
@@ -4,27 +4,17 @@ const { promisify } = require('util');
 
 const docId = '13yWi4XbloPgHJ48M1H4dDm_6WpJyhQif3iXZyCm8zfU';
 
-// var aluno = "";
-// var p1 = 0;
-// var p2 = 0;
-// var p3 = 0;
-// var media = 0;
-// var percentualfaltas;
-// var totaldeaula = 0;
-// var totaldefaltas = 0;
+var aluno = "";
+var p1 = 0;
+var p2 = 0;
+var p3 = 0;
+var media = 0;
+var percentualfaltas;
+var NotaFinal = 0;
+var Situacao;
+var NotaF;
 
-// media = (p1 + p2 + p3) / 3;
-// console.log(aluno + " Sua media e " + media);
-
-// percentualfaltas = (totaldefaltas * 100) / totaldeaula;
-// console.log(aluno+" seu percentual de faltas é "+percentualfaltas);
-
-// if ((media >= 7) && (percentualfaltas <= 25)) {
-//   console.log('Parabéns! Você foi aprovado');
-// } else {
-//   console.log('Que pena! Você foi reprovado');
-// }
-
+console.error('Aluno | Faltas | P1 | P2 | P3 | Situação | Nota para Aprovação Final');
 const accessSheet = async () => {
   const doc = new GoogleSpreadsheet(docId);
   await promisify(doc.useServiceAccountAuth)(credentials);
@@ -34,10 +24,52 @@ const accessSheet = async () => {
 
   });
   rows.forEach(row => {
-    console.log(row._cokwr, row._cpzh4, row._cre1l, row._chk2m, row._ciyn3, row._ckd7g, row._clrrx);
+    //limpar linhas undefined
+    aluno = row._cokwr == undefined ? "" : row._cokwr;
+    faltas = row._cpzh4 == undefined ? "" : row._cpzh4;
+    p1 = row._cre1l == undefined ? "" : row._cre1l;
+    p2 = row._chk2m == undefined ? "" : row._chk2m;
+    p3 = row._ciyn3 == undefined ? "" : row._ciyn3;
+    Situacao = row._ckd7g == undefined ? "" : row._ckd7g;
+    NotaFinal = row._clrrx == undefined ? "" : row._clrrx;
+
+    media = (p1 + p2 + p3) / 3;
+
+    media = parseInt(media);
+
+    percentualfaltasGui = (faltas * 100) / 60;
+
+    percentualfaltas = parseInt(faltas / 60 * 100);
+
+    if (media < 5) {
+
+      Situacao = 'Reprovado';
+
+      NotaF = NotaFinal;
+
+    } else if (media <= 5 && media < 7) {
+
+      Situacao = 'Exame Final';
+
+      NotaF = NotaFinal;
+
+    } else if ((media >= 7) && (percentualfaltas <= 25)) {
+
+      Situacao = "Aprovado";
+
+      NotaF = 0;
+
+    } else {
+
+      Situacao = "Reprovado por falta";
+
+      NotaF = NotaFinal;
+
+    }
+    console.log(aluno + ' | ' + faltas + ' | ' + p1 + ' | ' + p2 + ' | ' + p3 + ' | ' + Situacao + ' | ' + NotaFinal);
   });
 
-  //console.log(rows);
-  //console.log(info);
+  // console.log(rows);
+  //console.log('('+info+')');
 }
 accessSheet();
